687cbb0ac9950416f8aa8d1c11ed8cc73be88338
Camshaft54 <cam.shaw1234@gmail.com>
2020-04-22 20:35:00 -0700

    add Encryption v1.0

---
+++ Main.py
@@ -0,0 +1,7 @@
+user_input = input("Enter text: ")
+user_input = user_input.lower()
+total = 0
+for i in range(len(user_input)):
+    n = ord(user_input[i]) - 96
+    total += (26 ** i) * n
+print("Encrypted text: " + total)
